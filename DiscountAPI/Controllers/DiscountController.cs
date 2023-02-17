ad16fea6c9881bdb603aab47a7857110e780428d
kadirergul <kadir.ergul37@gmail.com>
2023-02-17 22:08:37 +0300

    controllers updated

---
+++ DiscountAPI/Controllers/DiscountController.cs
@@ -0,0 +1,27 @@
+﻿using Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscountAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly ILogger<DiscountController> _logger;
+
+        public DiscountController(ILogger<DiscountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("GetDiscountAppliedInvoice")]
+        public ActionResult<Invoice> GetDiscountAppliedInvoice([FromBody]Invoice request)
+        {
+            //_logger.Log(LogLevel.Information, Request.QueryString.Value);
+
+            return Ok("Hello API");
+        }
+    }
+}
